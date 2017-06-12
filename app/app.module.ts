a48553cb7f2469d8a911e51f88e7172f923e3661
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-12 19:16:40 +0100

    Create static card component

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
     imports: [
         BrowserModule
     ],
     declarations: [
-        AppComponent
+        AppComponent,
+        CardComponent
     ],
     bootstrap: [
         AppComponent
